6abe179c394e1fdbd436b64ec4c2853bfff3efcf
Tigran Oganesyan <t-oganesyan00@yandex.ru>
2020-05-06 13:36:23 +0300

    installation gulp-sass

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var {src, dest, watch}        = require('gulp');
+var sass        = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var reload      = browserSync.reload;
+
+// Save a reference to the `reload` method
+
+// Watch scss AND html files, doing different things with each.
+function bs() {
+
+    serveSass();
+    browserSync.init({
+        server: {
+            baseDir: "src"
+        }
+    });
+
+    watch("./src/*.html").on("change", reload);
+    watch("./src/sass/*.sass", serveSass);
+};
+
+function serveSass() {
+  return src("./src/sass/*.sass")
+      .pipe(sass())
+      .pipe(dest("src/css"))
+      .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
